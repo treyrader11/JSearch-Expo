8df11f1f6e166570dd792246af377b693568fe43
‘Trey <treyrdr09@gmail.com>
2023-06-26 16:20:54 -0500

    clean up

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -16,7 +16,7 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
               ? item.employer_logo
               : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
-          resizeMode='contain'
+          resizeMode="contain"
           style={styles.logoImage}
         />
       </TouchableOpacity>
