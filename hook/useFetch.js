4500609bc8c7b9c97e82e6102524324f3f48b809
‘Trey <treyrdr09@gmail.com>
2023-06-26 16:06:51 -0500

    re installed dotenv, added default options, and cleanup

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { RAPID_API_KEY } from "@env";
+
+const rapidApiKey = RAPID_API_KEY;
+console.log('rapidApiKey', rapidApiKey);
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
@@ -10,7 +14,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": '',
+      "X-RapidAPI-Key": rapidApiKey,
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
@@ -26,7 +30,7 @@ const useFetch = (endpoint, query) => {
       setIsLoading(false);
     } catch (error) {
       setError(error);
-      console.log(error)
+      console.log(error);
     } finally {
       setIsLoading(false);
     }
